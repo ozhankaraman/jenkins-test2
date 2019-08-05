f8ddfef1a80b87a129426ef853cc8eff952d307e
ZebraStack <50148692+zebrastack@users.noreply.github.com>
2019-08-05 10:04:32 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,49 @@
-<html><title>ZebraStack DockerTest</title><body><h1>ZebraStack DockerTest</h1></body></html>
+<html>
+  <head>
+    <title>ZebraStack DockerTest</title>
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-store" />
+    <meta http-equiv="expires" content="-1" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <style type="text/css">
+        body {
+            background-color:#3686be;
+            color:#FFF;
+            border:0px;
+            margin:0px;
+            overflow:hidden;
+        }
+        .wrapper {
+            position:absolute;
+            top:15%;
+            left:0px;
+            right:0px;
+            width:100%;
+            height:auto;
+        }
+        .sammy {
+            float:left;
+            width:100%;
+            height:auto;
+            text-align:center;
+        }
+        .message {
+            float:left;
+            width:100%;
+            height:auto;
+            font-size:36px;
+            text-align:center;
+            font-family:Arial,Helvetica,sans-serif;
+            margin-top:25px;
+            color: #ffffff;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="wrapper">
+            <div class="sammy"><img src="https://www.zebrastack.co.uk/zebrastack-logo.png"/></div>
+            <div class="message">ZebraStack DockerTest</div>
+    </div>
+  </body>
+</html>
